912ad35b1feb2228ff29b2668e5c92ce74c0b5ce
MiikNa <miika.napari32@gmail.com>
2025-02-27 13:35:19 +0200

    rollback again

--- api/app.py
+++ api/app.py
@@ -224,4 +224,5 @@ def create_input_sequences_for_prediction(df, seq_length=24):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
